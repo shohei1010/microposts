abf70ce98a85c55d48af0b6ef406abad67adb4d8
shoheiyoshinaga <syohei-yoshinaga@united.jp>
2016-04-18 10:54:30 +0000

    satoshi

--- microposts/app/controllers/users_controller.rb
+++ microposts/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+  before_action :logged_in_user, only: [:edit, :update]
+   before_action :correct_user,   only: [:edit, :update]#これ追加！
+   
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,11 +19,41 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+ def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+   redirect_to @user , notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
+
+  
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :area, :email, :password, :password_confirmation)
   end
+  
+  # beforeフィルター
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  
 end
--- microposts/config/routes.rb
+++ microposts/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root to: 'static_pages#home'
   get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  get    'setting' , to: 'sessions#edit'
 
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy, :edit, :update]
 end
